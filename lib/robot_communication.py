3ddc2636413c9ac857bd340bacc663f6d8d28bcd
Thobias <thobiasl@hiof.no>
2025-12-07 01:08:45 +0100

    Navn git biblioteket

--- lib/robot_communication.py
+++ lib/robot_communication.py
@@ -1,8 +1,8 @@
 """
-robot_communication library
+A.T.L.A.BOT library
 
 This library provides the RobotSerial class fo communicating
-between RoboDK and an Ardino-controlled robot with serial port.
+between RoboDK and an Arduino-controlled robot with serial port.
 """
 import sys
 from time import sleep
@@ -12,16 +12,16 @@ from serial.serialutil import SerialException
 
 class RobotSerial:
     """Class to communicate with Arduino via serial port
-     Objects of this class tries to connect to the port when created,
-     the prot is different based on the operating system:
+     Objects of this class tries to connect to the port when
+     created, the prot is different based on the operating system:
         - Windows: 'COM5' or equivalent
         - Linux: '/dev/ttyACM0' or equivalent
-        - MacOS: '/dev/tty.usbmodemXXXX' or equivalent"""
+        - Mac-OS: '/dev/tty.usbmodemXXXX' or equivalent"""
     def __init__(self, port, tcp_speed=30, baudrate=115200, read_delay=0.1, write_delay=0.1):
-        try:
+        try: # trying to connect to the robot with Serial
             self.ser = serial.Serial(port, baudrate)
         except SerialException:
-            print("The robot is't connected")
+            print("The robot is not connected")
             sys.exit(0)
         self.RDK = Robolink()
         self.robot = self.RDK.Item('', ITEM_TYPE_ROBOT)
@@ -39,24 +39,7 @@ class RobotSerial:
         self.send_signal(home)
         print(f"Robot going to home: {home}")
         self.robot.MoveJ(home)
-
-    def joint_values_for_target(self, target):
-        """Retrieves all the angles of all the joints of the target and stores them in a list"""
-        outer_list = self.robot.SolveIK(target.Pose())
-        list_of_joints = outer_list.list()
-        try:
-            #Uses the enumerate function to create indexes on all elements in the list,
-            # in order to be able to change the current element.
-            for idx, i in enumerate(list_of_joints):
-                round_value = round(i, 2)
-                list_of_joints[idx] = round_value
-            if list_of_joints and len(list_of_joints) > 0:
-                return list_of_joints
-        except AttributeError:
-            print("Target is missing Pose() or the IK-result is invalid")
-
-        except Exception as e:
-            print(f"{e}")
+        self.read_signal()
 
     def MoveJ(self, target):
         """Sending the angles to all joints and moves on the simulation with MoveJ"""
@@ -66,18 +49,58 @@ class RobotSerial:
             self.read_signal()
             self.robot.MoveJ(joint_values)
         else:
-            print(f"Kunne ikke hente leddverdier for target '{joint_values}'.")
+            print(f"Could not get joint values from target {joint_values}.")
+
+    def Joints(self):
+        """Return the current joint position as a robodk.robomath.Mat()"""
+        return self.robot.Joints()
+
+    def Pose(self):
+        """Returns the relative pose of an object"""
+        return self.robot.Pose()
+
+    def setDO(self, io_bool):
+        """Sending gripper IO signals"""
+        self.send_signal(io_bool, "G")
+        self.read_signal()
+
+    def joint_values_for_target(self, target):
+        """Retrieves all the angles of all the joints of the target and stores them in a list"""
+        if str(target).startswith("RoboDK"): #checks if target is an direct object of robodk
+            outer_list = self.robot.SolveIK(target.Pose())
+        elif str(target).startswith("Pose"): #checks if target is a Pose objekt
+            outer_list = self.robot.SolveIK(target)
+        else:
+            outer_list = target
+
+        #list_of_joints = outer_list.list()
+        inner_list = outer_list.list()
+        try:
+            #Uses the enumerate function to create indexes on all elements in the list,
+            # in order to be able to change the current element.
+            list_of_joints = []
+            for i in inner_list:
+                round_value = round(i, 2)
+                list_of_joints.append(float(round_value))
+
+            if list_of_joints and len(list_of_joints) > 0:
+                return list_of_joints
+        except AttributeError:
+            print("Target is missing Pose() or the IK-result is invalid")
+
+        except Exception as e:
+            print(f"{e}")
 
     def robot_speed_control(self, speed):
         """Setts a speed for all jonits, the speed has to be between 1 and 50"""
-        under_speed_limet = 1
-        upper_speed_limet = 51
-        if speed <= under_speed_limet or speed >= upper_speed_limet:
-            print(f"The set robot speed is not between {under_speed_limet} and {upper_speed_limet}")
-            self.robot.setSpeed(50)
-            self.send_signal(50, "L")
+        under_speed_limit = 1
+        upper_speed_limit = 91
+        if speed <= under_speed_limit or speed >= upper_speed_limit:
+            print(f"The set robot speed is not between {under_speed_limit} and {upper_speed_limit}")
+            self.robot.setSpeed(30)
+            self.send_signal(30, "L")
             self.read_signal()
-            self.tcp_speed = 50
+            self.tcp_speed = 30
         else:
             self.robot.setSpeed(speed)
             self.send_signal(speed, "L")
@@ -89,7 +112,7 @@ class RobotSerial:
         self.read_signal()
 
     def correcting_robodk(self, encoder_data):
-        """"Moves the robot in RoboDK based on the encoder values from the real robot"""
+        """Moves the robot in RoboDK based on the encoder values from the real robot"""
         self.robot.setJoints(encoder_data)
 
     def read_signal(self):
@@ -107,6 +130,11 @@ class RobotSerial:
                         text_list.append(float(i))
                     print(f"Encoder values from robot: {text_list}")
                     self.correcting_robodk(text_list)
+                elif read_serial.startswith("Emergency"):
+                    print("------------------------")
+                    print("Robot in emergency stop!")
+                    print("------------------------")
+                    sys.exit(0)
                 else:
                     print(f"{read_serial}")
 
@@ -114,9 +142,6 @@ class RobotSerial:
         """Sends signals to the Arduino using Serial."""
         if message_type == "R": #Degrees for all the joints
             info = 'R' + str(info) + '\n'
-
-        elif message_type == "S": #Speed for all the joints
-            info = 'S' + str(info) + '\n'
 
         elif message_type == "L": #Robot Speed
             info = 'L' + str(info) + '\n'
@@ -134,8 +159,3 @@ class RobotSerial:
         """Closing the connection with Arduino"""
         if self.ser.is_open:
             self.ser.close()
-
-    def setDO(self, io_bool):
-        """Sending gripper IO signals"""
-        self.send_signal(io_bool, "G")
-        self.read_signal()
